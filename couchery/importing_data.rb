47b3f3697f95805a51471bf1161a4cd09f6f5060
Josh Cheek <josh.cheek@gmail.com>
2015-02-26 12:02:46 -0700

    Some progress with a view

--- couchery/importing_data.rb
+++ couchery/importing_data.rb
@@ -1,6 +1,72 @@
 # setup couch
 require 'couchrest'
+
 db = CouchRest.database!("http://127.0.0.1:5984/couchrest-test")
+
+# create a view that allows querying google.events via the google_event_id
+begin
+  puts "PRE FIRST GUESS"
+  db.get '_design/google'
+  puts "POST FIRST GUESS"
+rescue RestClient::ResourceNotFound
+  db.save_doc(
+    '_id' => "_design/google", # must be string, b/c that's how couchrest accesses it
+    views: {
+      events: {
+        map: 'function(doc) { if (doc.type === "google.events") emit(doc.google_event_id, doc); }',
+      },
+    },
+  )
+end
+
+# google.calendars
+#   calendar_id
+#   refresh token
+#     # https://developers.google.com/accounts/docs/OAuth2
+#     # Note: Save refresh tokens in secure long-term storage and continue to use them as long as they remain valid. Limits apply to the number of refresh tokens that are issued per client-user combination, and per user across all clients, and these limits are different. If your application requests enough refresh tokens to go over one of the limits, older refresh tokens stop working.
+
+# google.attendees
+#   id
+#   email
+#   display_name
+
+# google.event
+#   id
+#   title (summary in the json)
+#   calendar_id
+#   location
+#   html_link
+#   google_event_id ('ojair...')
+#   start_time (event.start_time # => 2015-02-13T07:00:00Z)
+#   end_time
+
+save_event = lambda do |event|
+  # this "update or create" totally doesn't work, and I'm getting duplicate events :/
+  view      = db.view 'google/events', key: event.id
+  row       = view['rows'][0] || {}
+  doc_attrs = row['value']    || {}
+
+  # if it dne, create a new doc
+  doc_attrs[:type]               = 'google.events'
+  doc_attrs[:start_time]         = event.start_time   # eg "2015-02-13T07:00:00Z"
+  doc_attrs[:end_time]           = event.end_time     # eg "2015-02-18T07:00:00Z"
+  doc_attrs[:google_event_id]    = event.id           # eg "abc123"
+  doc_attrs[:title]              = event.title        # eg "Welcome & Staff Intros"
+  doc_attrs[:location]           = event.location
+  doc_attrs[:calendar_html_link] = event.html_link    # eg "https://www.google.com/calendar/event?eid=dTFhYjAzMzJuYjNkYTdna282aTVuZnU4MGMgY2FzaW1pcmNyZWF0aXZlLmNvbV81OWs4bXNycmMyZGRoY3Y3ODd2dWJ2cDBzNEBn"
+  doc_attrs[:calendar_id]        = '??'               # going to need to get this off the key we created the db with, it doesn't seem to store this
+
+  # this is probably wrong, b/c its directly saving google calendar json in our db as if its our document
+  doc_attrs[:attendees]          = event.attendees    # => [{"email"=>"team@turing.io",
+                                                       #      "displayName"=>"Turing Team",
+                                                       #      "responseStatus"=>"needsAction"
+                                                       #    }]
+
+  response  = db.save_doc(doc_attrs)
+  couch_doc = db.get(response['id'])
+  couch_doc
+end
+
 
 # setup google calendar
 require 'google_calendar'
@@ -12,38 +78,8 @@ cal = Google::Calendar.new :client_id     => ENV['GOOGLE_CLIENT_ID'],
                            :redirect_url  => "http://localhost:3000" # this is what Google uses for 'applications'
 
 cal.login_with_refresh_token(ENV['GOOGLE_REFRESH_TOKEN'])
+calendar_events = cal.find_events_in_range Date.today.to_time, (Date.today + 1).to_time
 
-events = cal.find_events_in_range Date.today.to_time, (Date.today + 1).to_time
-event  = events.first
-event.start_time   # => "2015-02-13T07:00:00Z"
-event.end_time     # => "2015-02-18T07:00:00Z"
-event.id           # => "abc123"
-event.attendees    # => [{"email"=>"team@turing.io",
-                   #      "displayName"=>"Turing Team",
-                   #      "responseStatus"=>"needsAction"
-                   #    }]
-event.transparency # => "transparent"
-event.status       # => "confirmed"
-event.title        # => "Welcome & Staff Intros"
-event.location     # => nil
-require "pry"
-binding.pry
-exit!
-# create
-response = @db.save_doc({:key => 'value', 'another key' => 'another value'})
-# {"ok"=>true, "id"=>"e0d70033da0fad3707fed320bd5e5de2", "rev"=>"1-cbb61d1f90f7c01b273737702265b6c8"}
-
-# fetch by id
-doc = @db.get(response['id'])
-# #<CouchRest::Document _id: "e0d70033da0fad3707fed320bd5e5de2", _rev: "1-cbb61d1f90f7c01b273737702265b6c8", key: "value", another key: "another value">
-
-# update
-doc["boogie"] = true
-@db.save_doc(doc)
-# {"ok"=>true, "id"=>"e0d70033da0fad3707fed320bd5e5de2", "rev"=>"2-3b067cc9f01fdf25814445088403382c"}
-
-doc["_rev"]
-# "2-3b067cc9f01fdf25814445088403382c" <- notice it modified the doc _rev
-
-require 'pry'
-binding.pry
+calendar_events.each do |calendar_event|
+  couch_event = save_event.call calendar_event
+end
