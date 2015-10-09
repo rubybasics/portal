0b64481b1971e8e31a7c661781619b92d5982a4d
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-09 15:12:49 -0600

    split out models to separate files

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,16 +1,3 @@
-# jsl-portal-development=# select typelem from pg_type where typname = '_activity_type';
-#  typelem
-# ---------
-#    30660
-# (1 row)
-class PgEnum < ActiveRecord::Base
-  self.table_name  = 'pg_type'
-  self.primary_key = 'typarray'
-  def self.for(name)
-    where(typname: "_#{name}").first
-  end
-end
-
 # jsl-portal-development=# select * from pg_enum where enumtypid = 30660;
 #  enumtypid | enumsortorder | enumlabel
 # -----------+---------------+------------
@@ -23,15 +10,6 @@ end
 class ActivityType < ActiveRecord::Base
   self.table_name = 'pg_enum'
   activity_type_enum = PgEnum.for :activity_type
-  default_scope { where(enumtypid: activity_type_enum.typelem) }
-  def name
-    enumlabel
-  end
-end
-
-class StatusType < ActiveRecord::Base
-  self.table_name = 'pg_enum'
-  activity_type_enum = PgEnum.for :status_type
   default_scope { where(enumtypid: activity_type_enum.typelem) }
   def name
     enumlabel
