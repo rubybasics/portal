71427ae2d55fa67cb0a7c86c59a6b23f262dae8a
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-08 21:26:09 -0600

    Add test helper #todays_date

---
+++ spec/features/admin_adds_lesson_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin adds lesson to schedule', type: :feature do
+  it 'adds a lesson to the schedule' do
+    today_is '2015-08-10'
+
+    visit '/admin'
+    expect(current_path).to eq admin_path
+
+    click_link 'Today'
+    expect(current_path).to eq '/admin/today/2015-08-10'
+
+    click_link 'Add Activity'
+    expect(current_path).to eq '/admin/today/2015-08-10/activities/new'
+
+    fill_in 'Title', with: 'Ruby Object Model'
+    fill_in 'Location', with: 'Room A'
+    fill_in 'Instructor', with: 'Josh Cheek'
+    fill_in 'Cohort', with: '1508'
+    fill_in 'Start', with: '9:00'
+    fill_in 'Finsih', with: '12.00'
+    fill_in 'Content', with: 'Lets learn the **Object Model**'
+
+    click_link 'Create Activity'
+
+    expect(current_path).to eq '/admin/daily/2015-08-10'
+    expect(page).to have_content '1508'
+    expect(page).to have_content 'Ruby Object Model'
+  end
+end
