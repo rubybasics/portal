5bb1b1be376419a63d93428197ada1cd6b8db4ec
Josh Cheek <josh.cheek@gmail.com>
2015-10-09 15:04:13 -0600

    Reflect on the available enum values

--- app/models/models.rb
+++ app/models/models.rb
@@ -1,3 +1,45 @@
+# jsl-portal-development=# select typelem from pg_type where typname = '_activity_type';
+#  typelem
+# ---------
+#    30660
+# (1 row)
+class PgEnum < ActiveRecord::Base
+  self.table_name  = 'pg_type'
+  self.primary_key = 'typarray'
+  def self.for(name)
+    where(typname: "_#{name}").first
+  end
+end
+
+
+
+# jsl-portal-development=# select * from pg_enum where enumtypid = 30660;
+#  enumtypid | enumsortorder | enumlabel
+# -----------+---------------+------------
+#      30660 |             1 | daily_fact
+#      30660 |             2 | warmup
+#      30660 |             3 | lesson
+#      30660 |             4 | work_time
+#      30660 |             5 | homework
+# (5 rows)
+class ActivityType < ActiveRecord::Base
+  self.table_name = 'pg_enum'
+  activity_type_enum = PgEnum.for :activity_type
+  default_scope { where(enumtypid: activity_type_enum.typelem) }
+  def name
+    enumlabel
+  end
+end
+
+class StatusType < ActiveRecord::Base
+  self.table_name = 'pg_enum'
+  activity_type_enum = PgEnum.for :status_type
+  default_scope { where(enumtypid: activity_type_enum.typelem) }
+  def name
+    enumlabel
+  end
+end
+
 class Activity < ActiveRecord::Base
   has_many :activities_to_cohorts, class_name: 'ActivityToCohort'
   has_many :activities_to_instructors, class_name: 'ActivityToInstructor'
