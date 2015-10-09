b8cd1f6d65997e91617dd98073db86203e2a7f2e
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-10-09 15:08:15 -0600

    wip- add routes and actions

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -2,4 +2,29 @@ class Admin::ActivitiesController < ApplicationController
   def new
     @activity = Activity.new
   end
+
+  def create
+    require 'pry'; binding.pry
+  end
+
+  private
+
+  def activity_params
+    hash = params.require(:activity).permit(:title,
+                                            :start,
+                                            :finish,
+                                            :content,
+                                            location_ids: [],
+                                            instructor_ids: [],
+                                            cohorts_ids: [])
+
+    i_ids = params[:activity][:instructor_ids].keys
+    l_ids = params[:activity][:location_ids].keys
+    c_ids = params[:activity][:cohort_ids].keys
+
+    hash[:instructor_ids] = i_ids
+    hash[:location_ids]   = l_ids
+    hash[:cohort_ids]     = c_ids
+    hash
+  end
 end
