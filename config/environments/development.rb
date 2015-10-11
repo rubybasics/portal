9582e7a7226500db1682091e1d3b086c03c0f5cb
Josh Cheek <josh.cheek@gmail.com>
2015-10-11 06:19:39 -0600

    Set the default console to be pry

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  console do
+    require "pry"
+    config.console = Pry
+  end
 end
